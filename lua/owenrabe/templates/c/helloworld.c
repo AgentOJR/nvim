d3c7fb600209eb6af0ee4c3fdbcdb01ded12a1a5
AgentOJR <owen.rabe@icloud.com>
2024-07-05 01:52:16 -0400

    added neogen

---
+++ lua/owenrabe/templates/c/helloworld.c
@@ -0,0 +1,10 @@
+//{{_author_}}
+////[Program description]
+//{{_date_}}
+// 169069086
+#include <stdio.h>
+int main() {
+  // printf() displays the string inside quotation
+  printf("Hello, World!");
+  return 0;
+}
