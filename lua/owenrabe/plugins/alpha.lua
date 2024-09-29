25d76fbf13f204736be496998d319903972a8d4d
AgentOJR <owen.rabe@icloud.com>
2024-09-29 01:13:29 -0400

    added framework for custom splash page

--- lua/owenrabe/plugins/alpha.lua
+++ lua/owenrabe/plugins/alpha.lua
@@ -1,6 +1,20 @@
 return {
 	"goolord/alpha-nvim",
+	dependancies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		require("alpha").setup(require("alpha.themes.dashboard").config)
+		--local alpha = require("alpha")
+		--local dashboard = require("alpha.themes.startify")
+		--dashboard.section.header.val = {
+		--			[[			                                          ▄▄       ]],
+		--			[[▀███▄   ▀███▀                             ██                 ]],
+		--			[[ ███▄    █
+		--          [[  █ ███   █   ▄▄█▀██  ▄██▀██▄▀██▀   ▀██▀▀███ ▀████████▄█████▄  ]],
+		--		[[  █  ▀██▄ █  ▄█▀   ████▀   ▀██ ██   ▄█    ██   ██    ██    ██  ]],
+		--		[[  █   ▀██▄█  ██▀▀▀▀▀▀██     ██  ██ ▄█     ██   ██    ██    ██  ]],
+		--		[[  █     ███  ██▄    ▄██▄   ▄██   ███      ██   ██    ██    ██  ]],
+		--		[[▄███▄    ██   ▀█████▀ ▀█████▀     █     ▄████▄████  ████  ████▄]],
+		--	}
+		--		alpha.setup(dashboard.opts)
 	end,
 }
