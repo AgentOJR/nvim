dfb8b4abdedcdd73618fc815470c7b9bec3f5afc
AgentOJR <owen.rabe@icloud.com>
2025-01-10 12:55:53 -0500

    replaced several plugins with those from the "mini" collection of plugins

--- lua/owenrabe/plugins/lualine.lua
+++ lua/owenrabe/plugins/lualine.lua
@@ -1,13 +1,14 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		local lualine = require("lualine")
-		lualine.setup({
-			options = {
-				theme = "dracula",
-			},
-		})
-	end,
+	-- "nvim-lualine/lualine.nvim",
+	-- version = "*",
+	-- dependencies = "nvim-tree/nvim-web-devicons",
+	-- config = function()
+	-- 	local lualine = require("lualine")
+	-- 	lualine.setup({
+	-- 		options = {
+	-- 			theme = "dracula",
+	-- 		},
+	-- 	})
+	-- end,
+	{ "echasnovski/mini.statusline", version = "*" },
 }
