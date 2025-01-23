77a1f1c0ae7637679ddbecf257196e470bdf4747
AgentOJR <owen.rabe@icloud.com>
2025-01-23 11:55:47 -0500

    refactoring
    added documentation to custom keybindings

--- lua/owenrabe/core/keymaps.lua
+++ lua/owenrabe/core/keymaps.lua
@@ -2,52 +2,52 @@
 vim.g.mapleader = " "
 
 -- General
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
-vim.keymap.set("n", "<leader>mm", "<cmd>Alpha<cr>")
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear Search Highlighting" })
+vim.keymap.set("n", "<leader>mm", "<cmd>Alpha<cr>", { desc = "Open Alpha" })
 --Find
 
-vim.keymap.set("n", "<leader>fv", "<cmd>Oil<cr>")
+vim.keymap.set("n", "<leader>fv", "<cmd>Oil<cr>", { desc = "Open Oil file Browser" })
 
 --Telescope
 
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzily Find Files with Telescope" })
 
-vim.keymap.set("n", "<leader>fe", "<cmd>Telescope diagnostics<cr>")
-vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fe", "<cmd>Telescope diagnostics<cr>", { desc = "Fuzzily Find Errors with Telescope" })
+vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzily Find Words with Telescope" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Fuzzily Find Help with Telescope" })
 
 -- Conform Formatting
 vim.keymap.set("n", "<leader>f", "<cmd>lua require('conform').format()<cr>", { desc = "Format" })
 
 -- Start And End of File Jump (Big H and Big L)
-vim.keymap.set({ "n", "x", "o" }, "H", "^")
-vim.keymap.set({ "n", "x", "o" }, "L", "$")
+vim.keymap.set({ "n", "x", "o" }, "H", "^", { desc = "Jump to Start of File" })
+vim.keymap.set({ "n", "x", "o" }, "L", "$", { desc = "Jump to End of File" })
 
 --LSP Binds
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Hover" })
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Open Code Actions" })
 
 --Timewaste
-vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Make it Rain" })
 
 --Persistence
 -- load the session for the current directory
 vim.keymap.set("n", "<leader>qs", function()
 	require("persistence").load()
-end)
+end, { desc = "Load Session for CWD" })
 
 -- select a session to load
 vim.keymap.set("n", "<leader>qS", function()
 	require("persistence").select()
-end)
+end, { desc = "Select Session to Load" })
 
 -- load the last session
 vim.keymap.set("n", "<leader>ql", function()
 	require("persistence").load({ last = true })
-end)
+end, { desc = "Load Last Session" })
 
 -- stop Persistence => session won't be saved on exit
 vim.keymap.set("n", "<leader>qd", function()
 	require("persistence").stop()
-end)
+end, { desc = "Stop Persistence" })
