b20bc2561a752cdedd39d1bf9ec809417456977b
AgentOJR <owen.rabe@icloud.com>
2025-01-26 00:28:53 -0500

    Revert "disabled images for windows"
    This reverts commit 2ffb37156c1a40baa398798e0eab2d84f5052d24.

--- lua/owenrabe/plugins/images.lua
+++ lua/owenrabe/plugins/images.lua
@@ -1,7 +1,5 @@
 return {
-	-- "3rd/image.nvim",
-	-- build = true, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-	-- opts = {
-	-- 	enable = false,
-	-- },
+	"3rd/image.nvim",
+	build = true, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+	opts = {},
 }
