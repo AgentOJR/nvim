96551495eafc44b27867c4176097f7e9941c7ea2
AgentOJR <owen.rabe@icloud.com>
2025-01-31 09:53:15 -0500

    bug fix

--- init.lua
+++ init.lua
@@ -3,6 +3,4 @@ require("owenrabe.lazy")
 local separator = package.config:sub(0, 1)
 if separator == "\\" then
 	vim.o.shell = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
-elseif separator == "/" then
-	vim.o.shell = "/bin/fish"
 end
