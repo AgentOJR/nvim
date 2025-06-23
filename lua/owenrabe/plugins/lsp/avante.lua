3a01353af7f0424c942c49f9bafd00271fbf542e
AgentOJR <owen.rabe@icloud.com>
2025-06-23 14:14:16 -0400

    updated avante config

--- lua/owenrabe/plugins/lsp/avante.lua
+++ lua/owenrabe/plugins/lsp/avante.lua
@@ -11,11 +11,11 @@ return {
 		--
 		provider = "claude", -- Recommend using Claude
 		auto_suggestions_provider = "claude",
-		claude = {
-			endpoint = "https://api.anthropic.com",
-			model = "claude-3-7-sonnet-20250219",
-			temperature = 0,
-			max_tokens = 4096,
+		providers = {
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-3-7-sonnet-20250219",
+			},
 		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
